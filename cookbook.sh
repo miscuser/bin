393a3f776da11f566e27f7d7979289d3a59ed0e1
U-HFXLAPTOP-08\Fred.Jones <miscuser@gmail.com>
2016-03-25 10:58:13 -0500

    Added directory creation shortcuts.

--- cookbook.sh
+++ cookbook.sh
@@ -7,4 +7,10 @@ for f in *.mp4; do mv "$f" "`echo $f | sed s/Season\ //`"; done
 # Replace Episode with x0.
 for f in *.mp4; do mv "$f" "`echo $f | sed s/\ Episode\ /x0/`"; done
 
+# Create multiple directories at once.
+# This will create blah\testing and blah\dummy -- do not put a space after the commas 
+mkdir -p blah/{testing,dummy}
+# Multiple levels can be created at once.
+mkdir -p projects/{src,doc/{api,system},tools,db}
+
 
