cacb7d8997a8d89027b36dcf04be34981a940ffe
miscuser <miscuser@users.noreply.github.com>
2016-08-02 12:50:15 -0500

    Delete cshow.py

--- tests/cshow/cshow.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import csv
-sniffer = csv.Sniffer()
-
-def get_delimiter(string):
-    stripped_string = string.replace(" ", "")
-    dialect = sniffer.sniff(stripped_string)
-    return dialect.delimiter 
