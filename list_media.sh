85dcffd2a09e3546b78906e14cdd2f75ba5833f0
misclinux <miscuser@gmail.com>
2016-11-23 15:56:15 -0600

    Added books

--- list_media.sh
+++ list_media.sh
@@ -3,3 +3,4 @@
 
 tree /media/external/videos/Movies/ > /home/misc/Dropbox/movies.txt
 tree /media/external/videos/TV\ Shows/ > /home/misc/Dropbox/tv_shows.txt
+tree /home/misc/Dropbox/books/ > /home/misc/Dropbox/books.txt
